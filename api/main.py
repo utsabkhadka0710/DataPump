6cb3c06eeddc16389405fe626174398a14a045c3
Utsab Khadka <utsabkhadka9475@gmail.com>
2026-08-22 18:36:23 +0545

    feat: initialized the structure of API
    Created an FastAPI app with 3 endpoints
     1. /health (checks the health of the API)
     2. / (home)
     3. /jobs (both get_job & post_job)
    
    Just inialized the structure of the API.

---
+++ api/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+#fastapi app declearation
+app = FastAPI()
+
+class Job(BaseModel):
+    pass
+
+@app.get("/health")
+async def api_health_check():
+    return {
+        "message": "This is an API health checkpoint, and if you're seeing this message it means that the API is healthy and working properly."
+    }
+
+@app.get("/")
+async def home():
+    return {
+        "message": "Welcom to the home of the DataDump!",
+        "descrition": "This is an ongoing project that takes and processes the CSV file"
+        }
+
+@app.get("/jobs")
+async def get_job():
+    pass
+
+@app.post("/jobs")
+async def post_job():
+    pass
