d748406da53272f2cdf1fa0813b66627cfaf4b47
Utsab Khadka <utsabkhadka9475@gmail.com>
2026-09-03 21:32:55 +0545

    feat: initialized working api endpoints with fake_db setup

--- api/main.py
+++ api/main.py
@@ -1,7 +1,53 @@
+from uuid import uuid4
+from datetime import datetime
 from fastapi import FastAPI
 from pydantic import BaseModel
+from .models import (
+    JobStatus,
+    JobCreate,
+    JobUpdate,
+    JobResponse
+)
 
 
+class FakeDb():
+    def __init__(self):
+        self.items : dict = {}
+    
+    def create_job(self,job: JobCreate):
+        stored_job = JobResponse(
+            id = uuid4(),
+            source = job.source,
+            destination = job.destination,
+            batch_size= job.batch_size,
+                
+            status= JobStatus.PENDING,
+            processed_records = 0,
+            total_records= 0,
+            error_message= None,
+                
+            created_at = datetime.now(),
+            updated_at = None,
+            completed_at  = None
+        )
+        
+        self.items.update(
+            {
+                f"job {stored_job.id}" : stored_job
+            }
+        )
+        
+        # return self.items.get(f"{int(stored_job.id)}")
+        return self.items
+        
+    
+    def get_job(self,id=None):
+        if id is None:
+            return self.items
+        return self.items.get(f"job {id}")
+
+fake_db = FakeDb()   
+    
 #fastapi app declearation
 app = FastAPI()
 
@@ -18,10 +64,23 @@ async def home():
         "descrition": "This is an ongoing project that takes and processes the CSV file"
         }
 
+
 @app.get("/jobs")
-async def get_job():
-    pass
+async def get_all_job():
+    return fake_db.get_job()
+    
+@app.get("/jobs/{id}")
+async def get_job(id):
+    return fake_db.get_job(id=id)
 
 @app.post("/jobs")
-async def post_job():
-    pass
+async def post_job(job: JobCreate):
+    return fake_db.create_job(job)
+
+@app.put("/jobs/{id}")
+async def put_job():
+    return {"message":"the job put section is still under construction"}
+
+@app.patch("/jobs/{id}")
+async def patch_job():
+    return {"message":"the job patch section is still under construction"}
